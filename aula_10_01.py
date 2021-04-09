34c8fb205383806890c76ccfb41ac11127e31d9f
Urbano Santos <urbanolopes@gmail.com>
2021-04-08 22:33:04 -0300

    Create aula_10_01.py

---
+++ aula_10_01.py
@@ -0,0 +1,23 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import (
+    presence_of_element_located
+)
+
+
+url = 'https://selenium.dunossauro.live/aula_10_a.html'
+
+browser = Chrome(executable_path=r'./drivers/chromedriver.exe')
+
+browser.get(url)
+
+wdw = WebDriverWait(browser, 30)
+
+locator = (By.ID, 'request')
+
+wdw.until(
+    presence_of_element_located(locator)
+)
+
+print('Apareceu')
